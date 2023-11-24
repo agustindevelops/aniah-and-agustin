33dfd97b77566a46e154c3318f36f6d86ff4de62
Agustin <agustindevelops@gmail.com>
2023-11-24 15:33:25 -0600

    Christmas Background

--- src/app/page.js
+++ src/app/page.js
@@ -2,20 +2,31 @@ import PotluckForm from "@/components/PotluckForm";
 import PotluckItemList from "@/components/PotluckItems";
 
 import prisma from "@/prisma";
+import Image from "next/image";
 
 const Page = async () => {
   const potluckItems = await prisma.potluckItem.findMany();
 
   return (
-    <main className="flex flex-col min-h-screen p-4 sm:container mx-auto gap-4">
-      <h1 className="font-cursive text-4xl sm:text-5xl md:text-6xl lg:text-7xl xl:text-8xl font-bold text-red-600 text-center my-4">
-        Christmas Party Potluck 2023
-      </h1>
-      <div className="font-clean flex flex-col gap-4">
-        <PotluckForm />
-        <PotluckItemList potluckItems={potluckItems} />
+    <>
+      <div className="-z-10 fixed h-screen w-screen">
+        <Image
+          src="/christmas-background.png"
+          alt="christmas-background"
+          layout="fill"
+          objectFit="cover"
+        />
       </div>
-    </main>
+      <main className="flex flex-col min-h-screen p-4 sm:container mx-auto gap-4 parallax">
+        <h1 className="font-cursive text-4xl sm:text-5xl md:text-6xl lg:text-7xl xl:text-8xl font-bold text-white text-center my-4">
+          Christmas Party Potluck 2023
+        </h1>
+        <div className="font-clean flex flex-col gap-4">
+          <PotluckForm />
+          <PotluckItemList potluckItems={potluckItems} />
+        </div>
+      </main>
+    </>
   );
 };
 
